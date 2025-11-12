cfbdad75651ecc8cd524c39ce5e00a661fb455fa
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-12 11:35:03 +0530

    Define variables for resource group configuration

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,12 +1,20 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group to be created."
+  type        = string
+}
 
-  tags = merge(
-    {
-      Environment = var.environment
-      Owner       = var.owner
-    },
-    var.additional_tags
-  )
+variable "location" {
+  description = "The Azure region where the Resource Group will be created."
+  type        = string
+  default     = "centralindia"
+}
+
+variable "environment" {
+  description = "Specifies the deployment environment (e.g., dev, stg, prod)."
+  type        = string
+  default     = "dev"
+  validation {
+    condition     = contains(["dev", "stg", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, stg, or prod."
+  }
 }
