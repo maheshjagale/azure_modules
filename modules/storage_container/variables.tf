8bf1be9b547f9a572818b942e3b91b5bf0cc273e
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-12 11:49:31 +0530

    Create variables.tf

---
+++ modules/storage_container/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "Name of the storage container"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "container_access_type" {
+  description = "Access type for storage container"
+  type        = string
+}
