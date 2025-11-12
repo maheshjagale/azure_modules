2df0b7ea40f4d3847c81ae9204e4aa35f12959c3
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-12 11:51:26 +0530

    Create variables.tf

---
+++ modules/load_balancer/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "Name of the load balancer"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
