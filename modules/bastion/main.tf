46b550d21bf691fab24d7604faec6a93ea875ef1
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-13 11:26:45 +0530

    Create main.tf

---
+++ modules/bastion/main.tf
@@ -0,0 +1,21 @@
+# Create a public IP for the Bastion host
+resource "azurerm_public_ip" "bastion_pip" {
+  name                = "${var.bastion_name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Create the Bastion host
+resource "azurerm_bastion_host" "bastion" {
+  name                = var.bastion_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = var.subnet_id
+    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+  }
+}
